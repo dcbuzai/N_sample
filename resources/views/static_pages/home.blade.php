fdda87e411cd5938b5c18f59b59d97325146a755
dcbuzai <mywin7@qq.com>
2018-09-19 14:01:09 +0000

    routes and layouts

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 @section('title','首页')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Dc</h1>
+        <p class="lead">
+            你现在看到的是 Dc不在线上的首页
+        </p>
+        <p>
+            一切,将不会开始.
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">注册一个</a>
+        </p>
+    </div>
 @stop
